fa0593d175087f494b8d29599fae5dc16db7f410
JwaSangHeon <whk4051@naver.com>
2022-05-17 15:47:24 +0900

    리액트 라우터- URL파라미터

--- src/App.js
+++ src/App.js
@@ -1,96 +1,116 @@
+// import React from 'react';
+// import { useRef, useState, useCallback } from 'react';
+// import produce from 'immer';
+
+// const App = () => {
+//   const nextId = useRef(1);
+//   const [form, setForm] = useState({
+//     name: '',
+//     username: '',
+//   });
+//   const [data, setData] = useState({
+//     array: [],
+//     uselessValue: null,
+//   });
+
+//   //input 수정을 위한 함수
+//   const onChange = useCallback((e) => {
+//     const { name, value } = e.target;
+//     setForm(
+//       produce((draft) => {
+//         draft[name] = value;
+//       }),
+//     );
+//   }, []);
+
+//   const onSubmit = useCallback(
+//     (e) => {
+//       e.preventDefault();
+//       const info = {
+//         id: nextId.current,
+//         name: form.name,
+//         username: form.username,
+//       };
+
+//       //array에 새 항목 등록
+//       setData(
+//         // {
+//         //   ...data,
+//         //   array: data.array.concat(info),
+//         // }
+//         produce((draft) => {
+//           draft.array.push(info);
+//         }),
+//       );
+
+//       //form 초기화
+//       setForm({
+//         name: '',
+//         username: '',
+//       });
+//       nextId.current += 1;
+//     },
+//     [form.name, form.username],
+//   );
+
+//   const onRemove = useCallback((id) => {
+//     setData(
+//       // { ...data, array: data.array.filter((info) => info.id !== id) }
+//       produce((draft) => {
+//         draft.array.splice(
+//           draft.array.findIndex((info) => info.id === id),
+//           1,
+//         );
+//       }),
+//     );
+//   }, []);
+//   return (
+//     <>
+//       <form onSubmit={onSubmit}>
+//         <input
+//           name="username"
+//           placeholder="아이디"
+//           value={form.username}
+//           onChange={onChange}
+//         />
+//         <input
+//           name="name"
+//           placeholder="이름"
+//           value={form.name}
+//           onChange={onChange}
+//         />
+//         <button type="submit">등록</button>
+//       </form>
+//       <div>
+//         <ul>
+//           {data.array.map((info) => (
+//             <li key={info.id} onClick={() => onRemove(info.id)}>
+//               {info.username}({info.name})
+//             </li>
+//           ))}
+//         </ul>
+//       </div>
+//     </>
+//   );
+// };
+
+// export default App;
+
 import React from 'react';
-import { useRef, useState, useCallback } from 'react';
-import produce from 'immer';
+import { Routes, Route } from 'react-router-dom';
+import About from './9-day-react-router/pages/About';
+import Home from './9-day-react-router/pages/Home';
+import Profile from './9-day-react-router/pages/Profile';
 
 const App = () => {
-  const nextId = useRef(1);
-  const [form, setForm] = useState({
-    name: '',
-    username: '',
-  });
-  const [data, setData] = useState({
-    array: [],
-    uselessValue: null,
-  });
-
-  //input 수정을 위한 함수
-  const onChange = useCallback((e) => {
-    const { name, value } = e.target;
-    setForm(
-      produce((draft) => {
-        draft[name] = value;
-      }),
-    );
-  }, []);
-
-  const onSubmit = useCallback(
-    (e) => {
-      e.preventDefault();
-      const info = {
-        id: nextId.current,
-        name: form.name,
-        username: form.username,
-      };
-
-      //array에 새 항목 등록
-      setData(
-        // {
-        //   ...data,
-        //   array: data.array.concat(info),
-        // }
-        produce((draft) => {
-          draft.array.push(info);
-        }),
-      );
-
-      //form 초기화
-      setForm({
-        name: '',
-        username: '',
-      });
-      nextId.current += 1;
-    },
-    [form.name, form.username],
-  );
-
-  const onRemove = useCallback((id) => {
-    setData(
-      // { ...data, array: data.array.filter((info) => info.id !== id) }
-      produce((draft) => {
-        draft.array.splice(
-          draft.array.findIndex((info) => info.id === id),
-          1,
-        );
-      }),
-    );
-  }, []);
   return (
-    <>
-      <form onSubmit={onSubmit}>
-        <input
-          name="username"
-          placeholder="아이디"
-          value={form.username}
-          onChange={onChange}
-        />
-        <input
-          name="name"
-          placeholder="이름"
-          value={form.name}
-          onChange={onChange}
-        />
-        <button type="submit">등록</button>
-      </form>
-      <div>
-        <ul>
-          {data.array.map((info) => (
-            <li key={info.id} onClick={() => onRemove(info.id)}>
-              {info.username}({info.name})
-            </li>
-          ))}
-        </ul>
-      </div>
-    </>
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profiles/:username" element={<Profile />} />
+      </Routes>
+    </div>
   );
 };
 
